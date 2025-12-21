09467a0d2d94cb0e459035120b7153b93c202404
magnanimouskruppe <tryprivi@gmail.com>
2025-12-20 19:07:02 -0600

    Use redirect auth on mobile instead of popup

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider, signOut } from 'firebase/auth';
 
-// TODO: Replace with your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyC0EAcl_BFP8tnqYo0pv_yVDF1a_r9r3Hs",
   authDomain: "nfl-pickem-app-18eee.firebaseapp.com",
@@ -13,9 +12,18 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
+
 const provider = new GoogleAuthProvider();
 
-export const loginWithGoogle = () => signInWithPopup(auth, provider);
+// Use redirect on mobile, popup on desktop
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+export const loginWithGoogle = () => {
+  if (isMobile) {
+    return signInWithRedirect(auth, provider);
+  }
+  return signInWithPopup(auth, provider);
+};
+
 export const logout = () => signOut(auth);
-export { auth };
